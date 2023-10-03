631ca11ef9caea35bcb5c387a28d0c206f64fe0a
s10016635 <alexantone@icloud.com>
2023-10-03 10:32:35 -0500

    Started LoginPage

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 
-Builder.load_file("LoginPage.kv")
+Builder.load_file("LoginPage1.kv")
 class LoginPageApp(App):
     def build(self):
         return QuizManager()
@@ -26,10 +26,49 @@ class ErrorScreen(Screen):
         self.manager.current = "question2"
 
 class Question2Screen(Screen):
-    def answer_question(self, heart):
-        if heart.lower() == "deep in the heart of texas":
-            self.manager.current = "question1"
+    def answer_question(self, text):
+        if text.lower() == "deep in the heart of texas":
+            self.manager.current = "correct"
+        else:
+            self.ids.invalid.text = "Invalid guess.\n Try again"
+            self.ids.invalid.color = 1, .5, .5
+
+
+
+
+
+
+
+dict = {}
+
+
+class LoginPage1App(App):
+    def build(self):
+        return LoginManager()
+class LoginManager(ScreenManager):
+    pass
+
+class LoggedInScreen(Screen):
+    pass
+
+class MainScreen(Screen):
+    def press_button(self):
+        self.manager.current = "new_account"
+
+    def login(self, username1, password1):
+        pass
+
+class NewAccountScreen(Screen):
+    def create_account(self, dict, username2, password2, password3):
+        if username2 not in dict.items and password2 == password3:
+            dict[username2] = password2
+        elif username2 in dict.items:
+            self.ids.incorrect.text = "Username taken"
+        elif not password2 == password3:
+            self.ids.incorrect.text = "Passwords don't match"
+
+
 
 
 if __name__ == "__main__":
-    LoginPageApp().run()
+    LoginPage1App().run()
